0b6646da6dae77b01ad6290b26f5478cdc080f4b
姜天宇 <jiangtianyu@foundersc.com>
2020-01-06 20:51:30 +0800

    财报数据可查

--- zipline/examples/bqsj.py
+++ zipline/examples/bqsj.py
@@ -1,116 +1,132 @@
 #!/usr/bin/env python
-#
-# Copyright 2014 Quantopian, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-"""Dual Moving Average Crossover algorithm.
 
-This algorithm buys apple once its short moving average crosses
-its long moving average (indicating upwards momentum) and sells
-its shares once the averages cross again (indicating downwards
-momentum).
-"""
+import pandas as pd
+import numpy as np
+import datetime as dt
+import statsmodels.api as sm
+import copy
 
 from zipline.api import order_target, record, symbol
+from zipline.api import schedule_function, date_rules,time_rules
+from zipline.protocol import BarData
 from zipline.finance import commission, slippage
 
 
-def initialize(context):
-    context.sym = symbol('600809.XSHG')
-    context.i = 0
 
-    # Explicitly set the commission/slippage to the "old" value until we can
-    # rebuild example data.
-    # github.com/quantopian/zipline/blob/master/tests/resources/
-    # rebuild_example_data#L105
+def initialize(context):
+    context.i = 0
     context.set_commission(commission.PerShare(cost=.0075, min_trade_cost=1.0))
     context.set_slippage(slippage.VolumeShareSlippage())
 
+    context.count = 10
+    context.tot_periods = 12
+    context.days = 0
+    context.periods = 0
+    context.neu = False
+
+    context.black_list = {}
+    context.black_days = 40
+
+    timing_params(context)
+    schedule_function(handle_data,date_rules.every_day(),time_rules.market_open(hours=1,minutes=30))
+
+
+## 择时函数参数
+def timing_params(context):
+    context.n = 18
+    context.m = 600
+    context.s = 0.5
+    context.t = -0.5
+    context.init = True
+    context.ema_state = True
+    context.rsrs_state = True
+    context.LLT_state = True
+    '''
+    context.d1 = 8
+    context.d2 = 13
+    context.d3 = 21
+    '''
+    context.d0 = 5
+    context.d1 = 8
+    context.d2 = 13
+    context.d3 = 21
+
+    context.asset = '000001.XSHG'
+    context.position = 1.0
+
+    context.timing = 0
+    context.prev_timing = 0
 
 def handle_data(context, data):
+    print(data.current(symbol("000001.XSHE"),["price","last_traded","open","high","low","close","volume"]))
     # Skip first 300 days to get full windows
     context.i += 1
     if context.i < 300:
         return
-
-    # Compute averages
-    # history() has to be called with the same params
-    # from above and returns a pandas dataframe.
-    short_mavg = data.history(context.sym, 'price', 100, '1d').mean()
-    long_mavg = data.history(context.sym, 'price', 300, '1d').mean()
-
-    # Trading logic
-    if short_mavg > long_mavg:
-        # order_target orders as many shares as needed to
-        # achieve the desired number of shares.
-        order_target(context.sym, 100)
-    elif short_mavg < long_mavg:
-        order_target(context.sym, 0)
-
-    # Save values for later inspection
-    record(AAPL=data.current(context.sym, "price"),
-           short_mavg=short_mavg,
-           long_mavg=long_mavg)
+    print(context.i)
 
 
-# Note: this function can be removed if running
-# this algorithm on quantopian.com
-def analyze(context=None, results=None):
-    import matplotlib.pyplot as plt
-    import logbook
-    logbook.StderrHandler().push_application()
-    log = logbook.Logger('Algorithm')
-
-    fig = plt.figure()
-    ax1 = fig.add_subplot(211)
-    results.portfolio_value.plot(ax=ax1)
-    ax1.set_ylabel('Portfolio value (USD)')
-
-    ax2 = fig.add_subplot(212)
-    ax2.set_ylabel('Price (USD)')
-
-    # If data has been record()ed, then plot it.
-    # Otherwise, log the fact that no data has been recorded.
-    if ('AAPL' in results and 'short_mavg' in results and
-            'long_mavg' in results):
-        results['AAPL'].plot(ax=ax2)
-        results[['short_mavg', 'long_mavg']].plot(ax=ax2)
-
-        trans = results.ix[[t != [] for t in results.transactions]]
-        buys = trans.ix[[t[0]['amount'] > 0 for t in
-                         trans.transactions]]
-        sells = trans.ix[
-            [t[0]['amount'] < 0 for t in trans.transactions]]
-        ax2.plot(buys.index, results.short_mavg.ix[buys.index],
-                 '^', markersize=10, color='m')
-        ax2.plot(sells.index, results.short_mavg.ix[sells.index],
-                 'v', markersize=10, color='k')
-        plt.legend(loc=0)
-    else:
-        msg = 'AAPL, short_mavg & long_mavg data not captured using record().'
-        ax2.annotate(msg, xy=(0.1, 0.5))
-        log.info(msg)
-
-    plt.show()
 
 
-def _test_args():
-    """Extra arguments to use when zipline's automated tests run this example.
-    """
-    import pandas as pd
 
-    return {
-        'start': pd.Timestamp('2011', tz='utc'),
-        'end': pd.Timestamp('2013', tz='utc'),
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Note: this function can be removed if running
+# # this algorithm on quantopian.com
+# def analyze(context=None, results=None):
+#     import matplotlib.pyplot as plt
+#     import logbook
+#     logbook.StderrHandler().push_application()
+#     log = logbook.Logger('Algorithm')
+#
+#     fig = plt.figure()
+#     ax1 = fig.add_subplot(211)
+#     results.portfolio_value.plot(ax=ax1)
+#     ax1.set_ylabel('Portfolio value (USD)')
+#
+#     ax2 = fig.add_subplot(212)
+#     ax2.set_ylabel('Price (USD)')
+#
+#     # If data has been record()ed, then plot it.
+#     # Otherwise, log the fact that no data has been recorded.
+#     if ('AAPL' in results and 'short_mavg' in results and
+#             'long_mavg' in results):
+#         results['AAPL'].plot(ax=ax2)
+#         results[['short_mavg', 'long_mavg']].plot(ax=ax2)
+#
+#         trans = results.ix[[t != [] for t in results.transactions]]
+#         buys = trans.ix[[t[0]['amount'] > 0 for t in
+#                          trans.transactions]]
+#         sells = trans.ix[
+#             [t[0]['amount'] < 0 for t in trans.transactions]]
+#         ax2.plot(buys.index, results.short_mavg.ix[buys.index],
+#                  '^', markersize=10, color='m')
+#         ax2.plot(sells.index, results.short_mavg.ix[sells.index],
+#                  'v', markersize=10, color='k')
+#         plt.legend(loc=0)
+#     else:
+#         msg = 'AAPL, short_mavg & long_mavg data not captured using record().'
+#         ax2.annotate(msg, xy=(0.1, 0.5))
+#         log.info(msg)
+#
+#     plt.show()
+
+
